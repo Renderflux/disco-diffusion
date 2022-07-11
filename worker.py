074702294bf5550fc9b6cc448dbffd80224ce626
kajdev <zekewotring@gmail.com>
2022-07-11 00:03:00 -0800

    feat: upload images and more args

--- worker.py
+++ worker.py
@@ -1,4 +1,5 @@
 import asyncio
+import base64
 import subprocess
 import sys
 import aiohttp
@@ -25,7 +26,11 @@ def construct_cmd(job):
     args.append(f"--batch_name {job['_id']}")
     args.append("--n_batches=1")
     args.append(f"--steps={job['steps']}")
-    args.append(f"--{job['model']} true")
+
+    for model, value in job['models'].items():
+        args.append(f"--{model} {value}")
+
+    args.append(f"--eta={job['eta']}")
     args.append(f"--clip_guidance_scale={job['clip_guidance_scale']}")
     args.append(f"--diffusion_model={job['diffusion_model']}")
     args.append(f"--clamp_max={job['clamp_max']}")
@@ -35,13 +40,20 @@ def construct_cmd(job):
     args.append(f"--set_seed={job['seed']}")
     args.append(f"--cut_innercut={job['cut_innercut']}")
     args.append(f"--cut_overview={job['cut_overview']}")
+    args.append(f"--use_secondary_model={job['use_secondary_model']}")
 
     return " ".join(args)
 
 async def update_job_progress(job):
     while True:
         async with aiohttp.ClientSession() as session:
-            async with session.post(f"{BASE}batches/{job['_id']}/progress", data=open(f"/workspace/images_out/{job['_id']}/progress.png", "rb")) as resp:
+
+            json = {
+                "progress": 0,
+                "image": base64.b64encode(open(f"/workspace/images_out/{job['_id']}/progress.png", "rb").read()).decode("utf-8")
+            }
+
+            async with session.post(f"{BASE}jobs/{job['_id']}/progress", json=json) as resp:
                 if resp.status != 200:
                     print(f"Error sending progress data to API...")
                     await asyncio.sleep(JOB_FAIL_WAIT)
@@ -54,7 +66,7 @@ async def run_job():
 
     progress_task = asyncio.create_task(update_job_progress(job))
     
-    cmd = construct_cmd(job)
+    cmd = construct_cmd(job['settings'])
     process = asyncio.create_subprocess_shell(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     await process.wait()
 
@@ -62,14 +74,25 @@ async def run_job():
 
     if process.returncode != 0:
         print(f"Error with job...")
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(f"{BASE}jobs/{job['_id']}/fail", json={"error": "job failed"}) as resp:
+                if resp.status != 200:
+                    print(f"Error sending fail data to API...")
+                    await asyncio.sleep(JOB_FAIL_WAIT)
+                    return
+                print(f"Sent fail data to API...")
+
         await asyncio.sleep(JOB_FAIL_WAIT)
         return
 
-    filepath = f"/workspace/images_out/{job['_id']}/{job['_id']}(0)_0.png"
+    json = {
+        "image": base64.b64encode(open(f"/workspace/images_out/{job['_id']}/{job['_id']}(0)_0.png", "rb").read()).decode("utf-8")
+    }
 
     # send the file data to the API when the job completes
     async with aiohttp.ClientSession() as session:
-        async with session.post(f"{BASE}batches/{job['_id']}/complete", data=open(filepath, "rb")) as resp:
+        async with session.post(f"{BASE}batches/{job['_id']}/complete", json=json) as resp:
             if resp.status != 200:
                 print(f"Error sending file data to API...")
                 await asyncio.sleep(JOB_FAIL_WAIT)
