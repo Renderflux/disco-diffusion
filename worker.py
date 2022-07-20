3444478e70a18c3eedb7f34c06226a2901ae0786
kajdev <zekewotring@gmail.com>
2022-07-19 20:11:04 -0800

    feat: support weighted prompts

--- worker.py
+++ worker.py
@@ -59,7 +59,16 @@ async def fetch_job():
 def construct_cmd(job, _id):
     args = ["python disco.py"]
 
-    args.append("--text_prompt \"{\\\"0\\\": [\\\""+job['prompt']+"\\\"]}\"")
+    if job.get("prompts"):
+        prompts = []
+
+        for prompt in job["prompts"]:
+            prompts.append(f"\\\"{prompt['prompt']}:{prompt['weight']}\\\"")
+
+        args.append("--text_prompt \"{\\\"0\\\": ["+', '.join(prompts)+"]}\"")
+    else:
+        args.append("--text_prompt \"{\\\"0\\\": [\\\""+job['prompt']+"\\\"]}\"")
+
     args.append(f"--width_height \"[{job['width']}, {job['height']}]\"")
     args.append(f"--batch_name {_id}")
     args.append("--n_batches=1")
